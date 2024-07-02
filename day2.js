d175e1030083d84fcbbcf78246e8565d66426158
biggie06 <129962161+biggie06@users.noreply.github.com>
2024-07-02 09:41:06 +0100

    Add files via upload

---
+++ day2.js
@@ -0,0 +1,9 @@
+var myDate = new Date();
+ var months = ["January", "February","March","April","May","June","July","August","September","October","November","December"];
+ var month =myDate.getMonth();
+ var day = myDate.getDay();
+ 
+console.log(myDate);
+console.log(months[month]);
+console.log(day);
+
